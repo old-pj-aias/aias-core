07134d6275270a03409eb73dbc390a7b1fbda89f
akakou <akakou571@gmail.com>
2020-08-26 00:50:13 +0900

    [add] Convert rust str to C str

--- src/utils.rs
+++ src/utils.rs
@@ -2,12 +2,16 @@ use std::ffi::{CString, CStr};
 use std::os::raw::c_char;
 
 
-pub fn get_c_string(to: *const c_char) -> String{
+pub fn from_c_str(to: *const c_char) -> String{
     let c_str = unsafe { CStr::from_ptr(to) };
     match c_str.to_str() {
         Err(_) => "".to_string(),
         Ok(string) => string.to_string(),
     }
+}
+
+pub fn to_c_str(from: String) ->  *mut c_char{
+    CString::new(from).unwrap().into_raw()
 }
 
 pub fn free(s: *mut c_char) {
