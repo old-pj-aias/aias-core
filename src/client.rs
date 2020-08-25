c2746bacd8b63f5f68e5feb905860f66f373eb1c
akakou <akakou571@gmail.com>
2020-08-25 17:10:54 +0900

    [add] Blind signature

--- src/client.rs
+++ src/client.rs
@@ -1,14 +1,14 @@
 use std::os::raw::{c_char};
 use std::ffi::{CString, CStr};
 
-use fair_blind_signature::{EJPubKey, FBSParameters, FBSSender};
-use std::cell::RefCell; 
+use fair_blind_signature::{EJPubKey, FBSParameters, FBSSender, BlindedDigest};
+use std::cell::{RefCell, RefMut}; 
 
 use rand::rngs::OsRng;
 use rsa::{BigUint, PublicKey, RSAPrivateKey, RSAPublicKey, PaddingScheme, PublicKeyParts};
 
 
-
+#[derive(Clone)]
 struct TestCipherPubkey {}
 
 impl EJPubKey for TestCipherPubkey {
@@ -20,6 +20,8 @@ impl EJPubKey for TestCipherPubkey {
         return message;
     }
 }
+
+
 
 thread_local!(static ODB: RefCell<Option<FBSSender<TestCipherPubkey>>> = RefCell::new(None)); 
 
@@ -39,16 +41,29 @@ pub fn new() {
     };
 
     ODB.with(|odb_cell| { 
-        let mut odb = odb_cell.borrow_mut().as_mut(); 
-        odb = Some(&mut FBSSender::new(parameters));
+        let mut odb = odb_cell.borrow_mut();
+        *odb = Some(FBSSender::new(parameters));
     }); 
 }
 
 pub fn destroy() {
     ODB.with(|odb_cell| { 
-        let mut odb = odb_cell.borrow_mut().as_mut(); 
-        odb = None;
+        let mut odb = odb_cell.borrow_mut(); 
+        *odb = None;
     }); 
+}
+
+pub fn blind(message: String) -> String {
+    let mut serialized = "".to_string();
+
+    ODB.with(|odb_cell| { 
+        let mut odb  = odb_cell.borrow_mut().as_mut().unwrap().clone();
+        let (digest, _ , _, _) = odb.blind("".to_string()).unwrap();
+
+        serialized = serde_json::to_string(&digest).unwrap();
+    });
+
+    serialized
 }
 
 
@@ -62,9 +77,9 @@ pub extern fn init_aias_ios_free() {
     destroy();
 }
 
-
 #[test]
 fn test_init_and_destroy() {
     new();
+    blind("aaa".to_string());
     destroy();
 }
