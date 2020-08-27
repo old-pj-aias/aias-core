a788abcadb1930302e029e29faba688f3404e7e8
akakou <akakou571@gmail.com>
2020-08-27 12:03:38 +0900

    [add] Aias new for Android

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,18 @@ mod sender;
 mod signer;
 mod verifyer;
 mod tests;
+
+use jni::objects::{JClass, JString};
+use jni::JNIEnv;
+use jni::sys::jstring;
+
+#[no_mangle]
+pub extern "system" fn Java_com_aias_aias_Aias_new(env: JNIEnv,
+                                             class: JClass,
+                                             input: JString) {
+    let input: String = 
+        env.get_string(input).expect("Couldn't get java string!").into();
+
+    sender::new(input);
+ }
+
