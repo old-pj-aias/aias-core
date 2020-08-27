fd65bc53ca53de02f443f222c3eeaf85b5c26f9e
akakou <akakou571@gmail.com>
2020-08-27 12:26:56 +0900

    [add] Blind function for Android

--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,18 @@ pub extern "system" fn Java_com_aias_aias_Aias_new(env: JNIEnv,
     sender::new(input);
  }
 
+#[no_mangle]
+pub extern "system" fn Java_com_aias_aias_Aias_blind(env: JNIEnv,
+                                             class: JClass,
+                                             input: JString) -> jstring {
+    let input: String = 
+        env.get_string(input).expect("Couldn't get java string!").into();
+
+    let output = sender::blind(input);
+
+    let output = env.new_string(format!("Hello, {}!", output))
+        .expect("Couldn't create java string!");
+
+    output.into_inner()
+ }
+
