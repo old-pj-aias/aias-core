5da76fab000d87f6fa4833c7865f45f9c1b1bb6e
akakou <akakou571@gmail.com>
2020-08-30 15:55:16 +0900

    [change] Argument type

--- core/src/verifyer.rs
+++ core/src/verifyer.rs
@@ -16,12 +16,10 @@ thread_local!(static ODB: RefCell<Option<FBSVerifyer<RSAPubKey>>> = RefCell::new
 
 
 #[no_mangle]
-pub fn new_verifyer(signer_pubkey: *const c_char, judge_pubkeys: *const c_char) {
-    let signer_pubkey = utils::from_c_str(signer_pubkey);
+pub fn new_verifyer(signer_pubkey: String, judge_pubkeys: String) {
     let signer_pubkey = pem::parse(signer_pubkey).expect("failed to parse pem");
     let signer_pubkey = RSAPublicKey::from_pkcs8(&signer_pubkey.contents).expect("failed to parse pkcs8");
 
-    let judge_pubkeys = utils::from_c_str(judge_pubkeys);
     let judge_pubkeys = pem::parse(judge_pubkeys).expect("failed to parse pem");
     let judge_pubkeys = RSAPublicKey::from_pkcs8(&judge_pubkeys.contents).expect("failed to parse pkcs8");
 
@@ -54,19 +52,16 @@ pub fn destroy_verifyer() {
 
 
 #[no_mangle]
-pub fn verify(signature: *const c_char, message: *const c_char) -> bool {
+pub fn verify(signature: String, message: String) -> bool {
     let mut is_vailed = false;
 
-    let message = utils::from_c_str(message);
-    
-    let signature = utils::from_c_str(signature);
     let signature: Signature = serde_json::from_str(&signature).expect("Parsing json error");
 
     ODB.with(|odb_cell| { 
         let mut odb = odb_cell.borrow_mut();
         let verifyer = odb.as_mut().unwrap();
        
-        is_vailed = verifyer.verify(signature, message).unwrap();
+        is_vailed = verifyer.verify(signature, message);
     });
 
     is_vailed
