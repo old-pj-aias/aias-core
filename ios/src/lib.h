f8c5e98dcd63873d8f2eae097d0bd592c9f829bf
shunta nakajima <nakg_asimo@icloud.com>
2020-08-31 01:55:33 +0900

    write const

--- ios/src/lib.h
+++ ios/src/lib.h
@@ -1,13 +1,13 @@
 #include <stdint.h>
 
-char *blind_ios(const char *to);
+const char *blind_ios(const char *to);
 
 void destroy_ios();
 
-char *generate_check_parameter_ios();
+const char *generate_check_parameter_ios();
 
 void new_ios(const char *signer_pubkey, const char *judge_pubkeys);
 
 void set_subset_ios(const char *to);
 
-char *unblind_ios(const char *to);
+const char *unblind_ios(const char *to);
