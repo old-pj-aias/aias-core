dbd3ccac21f15cf17090a600decc3b8f6c491dba
kino-ma <kino.ma.ms@gmail.com>
2020-09-01 15:17:06 +0900

    [update] C Header file correspondig to Rust

--- ios/src/lib.h
+++ ios/src/lib.h
@@ -6,7 +6,7 @@ void destroy_ios();
 
 const char *generate_check_parameter_ios();
 
-void new_ios(const char *signer_pubkey, const char *judge_pubkeys);
+void new_ios(const char *signer_pubkey, const char *judge_pubkeys, unsigned int id);
 
 void set_subset_ios(const char *to);
 
